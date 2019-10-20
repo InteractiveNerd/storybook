754aff783b549014b6b456a37323fe1b25112124
Kiwane Garner <kiwanegarner012@yahoo.com>
2019-10-20 19:59:18 -0400

    Currently working on the Composite components section of the Learn Storybook guide/docs

--- kg-storybook/.storybook/config.js
+++ kg-storybook/.storybook/config.js
@@ -1,7 +1,21 @@
 import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
+import '../src/index.css';
+
+const req = requireContext('../src/components', true, /\.stories.js$/);
 
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
+
+
+// import { configure } from '@storybook/react';
+
+// function loadStories() {
+//   require('../src/stories');
+// }
+
+// configure(loadStories, module);
